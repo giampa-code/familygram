4e6f4aa179801ce0a48a7ee781285035f088e9d3
giampa95 <giampa95@hotmail.com>
2022-01-18 10:21:43 -0300

    class 28

--- users/urls.py
+++ users/urls.py
@@ -5,10 +5,10 @@ from django.urls import path
 
 # Views
 from users import views as users_views
-from django.views.generic import TemplateView
+#from django.views.generic import TemplateView
 
 urlpatterns = [
-    path(route='profile/<str:username>/', view=TemplateView.as_view(template_name='users/detail.html'), name='detail' ),
+    path(route='profile/<str:username>/', view=users_views.UserDetailView.as_view(), name='detail' ),
     path(route='login', view=users_views.login_view, name='login'),
     path(route='logout', view=users_views.logout_view, name='logout'),
     path(route='signup', view=users_views.signup_view, name='signup'),
